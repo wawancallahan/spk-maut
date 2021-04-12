428799aff4d3d95e5246e689b2f6dc6e53d25542
wawancallahan <wawan.callahan@gmail.com>
2021-04-13 00:15:38 +0800

    U : Step Number

--- tambah_kriteria.php
+++ tambah_kriteria.php
@@ -96,7 +96,7 @@ ob_start();
 
                                 <div class="form-group">
                                     <label>Bobot</label>
-                                    <input type="number" name="bobot" class="form-control" placeholder="Bobot" min="0" value="0" required>
+                                    <input type="number" name="bobot" class="form-control" placeholder="Bobot" min="0" step="0.1" value="0" required>
                                 </div>
 
                                 <div class="form-group">
